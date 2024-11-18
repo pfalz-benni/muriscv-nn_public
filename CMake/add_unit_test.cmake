573ac7babbbed659c7039cda3602cd5403279669
Benedikt Witteler <benedikt-witteler@gmx.de>
2024-11-18 17:46:08 +0100

    Research internship: Adapt library for running it on the K230 hardware platform:    - K230 runs RT-Thread as an RTOS    - 2-step build first building the mursicv-nn library and then benchmark code with different versions of GCC    - Build unit tests as well as TFLM integration tests for benchmarking

--- CMake/add_unit_test.cmake
+++ CMake/add_unit_test.cmake
@@ -31,6 +31,9 @@ macro(add_muriscv_nn_unit_test TEST_NAME)
             ${PROJECT_SOURCE_DIR}/Tests/TestCases/${TEST_FILE_NAME}/${TEST_FILE_NAME}.c)
     endif()
 
+    if(NOT ENABLE_LIB_BUILD)
+        target_include_directories(${TEST_NAME} PUBLIC ${PROJECT_SOURCE_DIR}/Include)
+    endif()
     target_link_libraries(${TEST_NAME} PUBLIC muriscvnn unity)
 
     # Register test with CTest and provide command to execute
